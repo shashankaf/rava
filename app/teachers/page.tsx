b177eb7d937556cb05096beeaad5013a88a6b0f1
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-05-16 22:31:14 +0300

    teachers page

--- app/teachers/page.tsx
+++ app/teachers/page.tsx
@@ -1,11 +1,36 @@
-import Slider from "@/components/Slider"
-import React from "react"
+import React from "react";
+import { supabase } from "@/utils/supabase/server";
+import SingleTeacher from "@/components/teachers/SingleTeacher";
+import Heading from "@/components/Heading";
 
-const Teachers = () => {
+const fetchTeachers = async () => {
+  const { error, data } = await supabase.from("teacher").select();
+  if (error) {
+    throw Error;
+  }
+  return data;
+};
+async function TeachersPage() {
+  const teachers = await fetchTeachers();
   return (
-  <>
-  </>
-  )
+    <div className="mt-24">
+      <Heading text="مامۆستایانی راڤە" />
+      <div className="flex flex-row justify-center flex-wrap gap-4">
+        {teachers.map((item) => {
+          return (
+            <div>
+              <SingleTeacher
+                title={item.name}
+                alt={item.name}
+                image={item.photo}
+                specialty={item.specialty}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Teachers
+export default TeachersPage;
--- components/teachers/SingleTeacher.tsx
+++ components/teachers/SingleTeacher.tsx
@@ -4,15 +4,16 @@ import React from "react"
 interface SingleProps {
   image: string,
   alt: string,
-  title: string
+  title: string,
+  specialty: string,
 }
-const SingleTeacher = ({image, alt, title}: SingleProps) => {
+const SingleTeacher = ({image, alt, title, specialty}: SingleProps) => {
   return (
-    <article className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto mt-24">
-      <Image src={image} alt={alt} className="absolute inset-0 h-full w-full object-cover" />
+    <article className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl px-8 pb-8 pt-40 max-w-sm mx-auto">
+      <Image src={image} alt={alt} className="absolute inset-0 h-full w-full object-cover" width={400} height={600} />
         <div className="absolute inset-0 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
         <h3 className="z-10 mt-3 text-3xl font-bold text-white">{title}</h3>
-        <div className="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">City of love</div>
+        <div className="z-10 gap-y-1 overflow-hidden text-sm leading-6 text-gray-300">{specialty}</div>
     </article>
   )
 }
