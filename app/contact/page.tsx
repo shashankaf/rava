6fe7b314daca617673bcef298b00f1e30a010e2c
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-05-20 17:10:07 +0300

    bad url on contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,7 +6,7 @@ import localFont from "next/font/local";
 const rudaw = localFont({ src: "../rudaw.ttf" });
 
 const Contact = () => {
-  const insta_link =
+  const insta_link = "https://google.com"
     "https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.instagram.com%2Frava_institute%3Figsh%3DNXpkaGQweGZvZDZ4%26fbclid%3DIwZXh0bgNhZW0CMTAAAR28UQlXb9yY0o6k0XW68O5jB4vbltzNexlVzw00lmTPjGsfoMjGnGAsiZM_aem_Ad5HpPTMNk15DVDMBT_5C9iOqNcvEKra5wXL2xa1GwdL8b_vo-kJEmGpOzsxs9sC1Lih0TSsYooUx_rqCA1Hz873&h=AT2cXvq6WzE4YaqI1ss8nb_SWFx3WstFERAOsjXrh795tpYZUyiZfzXIKHRvlFu1mmnfNVhQbyZ6q9Frg4TRqbQh8kEnfhM1hUGshuVJBq_6krfbKVfy8vz7fBTMOhv9p9Zrz6q9";
   const rava_map =
     "https://maps.google.com/maps?q=rava+institute&t=&z=13&ie=UTF8&iwloc=&output=embed";
