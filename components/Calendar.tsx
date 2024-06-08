6ceeced468467af251ad7aaf79c1edc205c30558
Shalaw Fatah <shalaw.fatah@gmail.com>
2024-06-08 11:38:54 +0300

    calendar update

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,31 +1,60 @@
-"use client"
+"use client";
 import React, { useCallback, useState } from "react";
 import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
- 
-const events: any[] = []
+import localFont from "next/font/local";
+import { cal_header, monthNames, weekdayNames } from "../lib/calendar";
+
+const bbc = localFont({ src: "/../app/sarkar_bbc.ttf" });
+
+const events: any[] = [];
 
 const localizer = momentLocalizer(moment);
 
-export default function CalendarComponent() {
-  const [date, setDate] = useState(new Date())
-  const [view, setView] = useState(Views.WEEK)
+// Extend moment to include custom AM/PM
+moment.updateLocale('en', {
+  meridiem: (hour: number) => (hour < 12 ? 'بەیانی' : 'ئێوارە'),
+});
 
-  const onNavigate = useCallback((newDate: Date) => setDate(newDate), [setDate])
-  const onView = useCallback((newView: any) => setView(newView), [setView])
+export default function CalendarComponent() {
+  const [date, setDate] = useState(new Date());
+  const [view, setView] = useState(Views.WEEK);
+
+  const onNavigate = useCallback(
+    (newDate: Date) => setDate(newDate),
+    [setDate],
+  );
+  const onView = useCallback((newView: any) => setView(newView), [setView]);
+
+  const formats = {
+    weekdayFormat: (date: Date) => weekdayNames[moment(date).format("dddd")], // Customize day format
+    monthHeaderFormat: (date: Date) => monthNames[moment(date).format("MMMM")],
+    dayRangeHeaderFormat: ({ start, end }: { start: Date; end: Date }) =>
+      `${monthNames[moment(start).format("MMMM")]} ${moment(start).format("DD")} – ${monthNames[moment(end).format("MMMM")]} ${moment(end).format("DD")}`,
+    dayHeaderFormat: (date: Date) =>
+      `${weekdayNames[moment(date).format("dddd")]} ${moment(date).format("DD")} ${monthNames[moment(date).format("MMMM")]}`,
+    timeGutterFormat: (date: Date) =>
+      moment(date).format("h:mm A"), // Custom time format to include AM/PM
+    timeRangeStartFormat: (date: Date) =>
+      moment(date).hour(8).minute(0).format("h:mm A"), // Start time of range
+    timeRangeEndFormat: (date: Date) =>
+      moment(date).hour(22).minute(0).format("h:mm A"), // End time of range
+  };
 
   return (
-    <div className="height600">
+    <div className={`${bbc.className} height600`}>
       <Calendar
+        rtl={true}
         date={date}
         events={events}
         localizer={localizer}
         onNavigate={onNavigate}
         onView={onView}
         view={view}
+        messages={cal_header}
+        formats={formats}
       />
     </div>
-  )
+  );
 }
-
---
+++ lib/calendar.ts
@@ -0,0 +1,35 @@
+export const cal_header: {[key:string]: string} = {
+    day: "رۆژ",
+    week: "هەفتە",
+    month: "مانگ",
+    previous: "پێشتر",
+    next: "دواتر",
+    today: "ئەمڕۆ",
+    agenda: "کارنامە",
+  };
+
+
+export const monthNames: { [key: string]: string }= {
+  January: "کانونی دووەم",
+  February: "شوبات",
+  March: "ئازار",
+  April: "نيسان",
+  May: "ئایار",
+  June: "حوزەیران",
+  July: "تەمووز",
+  August: "ئاب",
+  September: "ئەیلول",
+  October: "تشرینی یەکەم",
+  November: "تشرینی دوەەم",
+  December: "کانونی یەکەم",
+};
+
+export const weekdayNames: {[key:string]: string} = {
+  Saturday: "شەممە",
+  Sunday: "یەکشەممە",
+  Monday: "دووشەممە",
+  Tuesday: "سێشەممە",
+  Wednesday: "چوارشەممە",
+  Thursday: "پێنجشەممە",
+  Friday: "هەینی"
+}
